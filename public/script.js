0c5e41be90e9af7fbd50ff3500c9cc173f7958e4
Dawood Sardar <dawoodsardar252@gmail.com>
2026-03-20 15:58:30 +0500

    feat(JS): get notes function

--- public/script.js
+++ public/script.js
@@ -1,4 +1,30 @@
+const noteForm = document.getElementById('noteFrom');
 const noteInput = document.getElementById('noteInput');
-const addNoteButton = document.getElementById('addNoteBtn');
-const noteForm = document.getElementById('noteForm');
-const notesList = document.getElementById('notesList');
+const notesList = document.getElementById('noteList');
+
+const API = '/notes';
+
+// Get notes function
+
+async function getNotes() {
+    const res = await fetch(API);
+    const notes = res.json();
+
+    notesList.innerHTML = '';
+
+    notes.forEach( note => {
+
+        const li = document.createElement('li');
+        li.textContent = note.text + '';
+
+        const delBtn = document.createElement('button');
+        delBtn.textContent = '❌'
+        delBtn.addEventListener('doubleClick', () => {
+            deleteNote(note.id); // call the delete note function when user double clicks 
+        });
+
+        li.appendChild(delBtn); // append delete button inside ul li [ul > li > delete]
+        notesList.appendChild(li); // append li inside ul [ul > li]
+
+    })
+}
