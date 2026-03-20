0bcc64a26466f75716f2eff0b26753cec645d4bc
Dawood Sardar <dawoodsardar252@gmail.com>
2026-03-20 16:33:55 +0500

    Completed frontend logic, added add, delet and load prev notes option

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 const noteForm = document.getElementById('noteFrom');
 const noteInput = document.getElementById('noteInput');
-const notesList = document.getElementById('noteList');
+const notesList = document.getElementById('notesList');
 
 const API = '/notes';
 
@@ -10,7 +10,7 @@ async function getNotes() {
     const res = await fetch(API);
     const notes = res.json();
 
-    notesList.innerHTML = '';
+    // notesList.innerHTML = '';
 
     notes.forEach( note => {
 
@@ -19,12 +19,55 @@ async function getNotes() {
 
         const delBtn = document.createElement('button');
         delBtn.textContent = '❌'
-        delBtn.addEventListener('doubleClick', () => {
+        delBtn.addEventListener('dblclick', () => {
             deleteNote(note.id); // call the delete note function when user double clicks 
         });
 
         li.appendChild(delBtn); // append delete button inside ul li [ul > li > delete]
         notesList.appendChild(li); // append li inside ul [ul > li]
 
-    })
+    });
+    
+};
+
+// Add notes function
+
+async function addNote(text) {
+
+    await fetch(API, {
+        method: "POST", 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify( {text: text} )
+    });
+
+    getNotes();
 }
+
+// Delete Note
+
+async function deleteNote(id) {
+
+    await fetch(`${API}/${id}`, {
+        method: 'Delete'
+    });
+
+    getNotes();
+}
+
+// Submit form 
+
+noteForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const text = input.value.trim();
+
+    if (!text) return;
+
+    addNote(text);
+
+    noteInput.value = '';
+});
+
+getNotes(); // call function on startup to load the notes
